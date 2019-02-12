b1ce05f745ec984faf38e3d20b1d2e45942225ab
vjragavan <vijay@onepeloton.com>
2019-02-11 22:05:24 -0500

    create component using create-class

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+var createClass = require('create-react-class');
 
-// jsx lets you use declarative syntax for creating elements
+var Component = createClass ({
+  render: function(){
+    return (
+      <div style={{ color: this.props.color }}>
+        <h1> {this.props.greeting}, {this.props.name}</h1>
+      </div>
+      );
+  }
+});
 
-//var h1Element = <h1> hello world !!! </h1>;
-
-//functional component
-var Component = function (props) {
-  return (
-    <div style={{ color: props.color }}>
-      <h1> {props.greeting}, {props.name}</h1>
-    </div>
-    );
-};
-
-// tell react what to display and where
-// the where is defined the public - index.html which is the page that is displayed
 ReactDom.render(
-  //React.createElement(component),
   <div>
     <Component greeting="Hello" name="Vijay" color="green"/>
     <Component greeting="Hello" name="Wright" color="red"/>
